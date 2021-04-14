6483f0f93935cb77a1d4bd6b69c620a2561eeffa
RanderGabriel <rander.oliveira@escolaemmovimento.com.br>
2021-04-14 13:27:20 -0300

    editando o bucket

--- s3-bucket/main.tf
+++ s3-bucket/main.tf
@@ -2,22 +2,24 @@ terraform {
   required_version = "0.14.10"
   required_providers {
     aws = {
-        source = "hashicorp/aws"
-        version = "3.36.0"
+      source  = "hashicorp/aws"
+      version = "3.36.0"
     }
   }
 }
 
 provider "aws" {
-    region = "sa-east-1"
-    profile = "terraform"
+  region  = "sa-east-1"
+  profile = "terraform"
 }
 
-resource "aws_s3_bucket" "b" {
-  bucket = "terraform-bucket-teste-rander"
+resource "aws_s3_bucket" "bucket-teste-terraform" {
+  bucket = "terraform-bucket-teste-rander-editado"
   acl    = "private"
   tags = {
     Name        = "My bucket"
     Environment = "Dev"
+    Owner       = "Rander Oliveira"
+    UpdatedAt   = "2021-04-14"
   }
 }
