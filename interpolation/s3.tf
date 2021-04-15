4d9f90e19b27f0597ea80632a43deb1753ff69e1
Rander Oliveira <rander.oliveira@escolaemmovimento.com.br>
2021-04-15 01:09:45 -0300

    understanding tfstate

--- interpolation/s3.tf
+++ interpolation/s3.tf
@@ -5,8 +5,19 @@ resource "aws_s3_bucket" "terraform_interpolation" {
 }
 
 resource "aws_s3_bucket_object" "terraform_interpolation" {
-  bucket = aws_s3_bucket.terraform_interpolation.bucket
-  key    = local.ip_file_path
-  source = local.ip_file_path
-  etag   = filemd5(local.ip_file_path)
+  bucket       = aws_s3_bucket.terraform_interpolation.bucket
+  key          = local.ip_file_path
+  source       = local.ip_file_path
+  etag         = filemd5(local.ip_file_path)
+  tags         = local.common_tags
+  content_type = "application/json"
+}
+
+resource "aws_s3_bucket_object" "random" {
+  bucket       = aws_s3_bucket.terraform_interpolation.bucket
+  key          = "config/${random_pet.bucket.id}"
+  source       = local.ip_file_path
+  etag         = filemd5(local.ip_file_path)
+  content_type = "application/json"
+
 }
